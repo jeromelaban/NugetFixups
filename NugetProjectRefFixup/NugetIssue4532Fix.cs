ed30534d27f7bb3f40f270057bd440c38a90ad08
Jérôme Laban <jerome.laban@nventive.com>
2017-02-23 22:44:58 -0500

    Add project files.

---
+++ NugetProjectRefFixup/NugetIssue4532Fix.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Build.Framework;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace NugetFixup
+{
+    public class NugetIssue4532Fix : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public string IntermediatePath { get; set; }
+
+        public override bool Execute()
+        {
+            string assetFilePath = Path.Combine(IntermediatePath, "project.assets.json");
+            var o = JObject.Parse(File.ReadAllText(assetFilePath));
+
+            var q = from compileNode in o.SelectTokens($"..compile").Concat(o.SelectTokens($"..runtime"))
+                    from child in compileNode.Children()
+                    where (child as JProperty)?.Name?.Contains("bin/placeholder") ?? false
+                    select compileNode.Parent;
+
+            q.Distinct().ToList().ForEach(c => c.Remove());
+
+            File.Move(assetFilePath, assetFilePath + ".bak");
+
+            File.WriteAllText(assetFilePath, o.ToString());
+
+            return true;
+        }
+    }
+}
